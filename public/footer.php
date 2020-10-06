b69f11a4219211fd4d09f16eab0e3f7646ed8f45
Timothée Lefebvre <292261@supinfo.com>
2020-10-06 16:16:04 +0200

    main n

---
+++ public/footer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<footer class="page-footer font-small cyan darken-3">
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 py-5">
+        <div class="mb-5 flex-center">
+
+          <a class="gplus-ic ico">
+            <img src="images/discord_ico.png" alt="">
+          </a>
+
+          <a class="li-ic ico">
+            <img src="images/insta_ico.png" alt="">
+          </a>
+
+          <a class="ins-ic ico">
+            <img src="images/snapchat_ico.png" alt="">
+          </a>
+
+          <a class="pin-ic ico">
+            <img src="images/youtube_ico.png" alt="">
+          </a>
+        </div>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row-->
+
+  </div>
+  <!-- Footer Elements -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+  </body>
+</html>
