36c35e3ee724860c50274e557c835193d48eae63
Timothée Lefebvre <292261@supinfo.com>
2020-10-06 16:36:26 +0200

    main n

--- public/footer.php
+++ public/footer.php
@@ -5,43 +5,22 @@
     <title></title>
   </head>
   <body>
-<footer class="page-footer font-small cyan darken-3">
+<footer class="bg-secondary">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-white text-capitalize font-weight-light">Nozdi Beats</h1>
+                <h3>Site de beatmaking et de productions musicales</h3>
+            </div>
 
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 py-5">
-        <div class="mb-5 flex-center">
-
-          <a class="gplus-ic ico">
-            <img src="images/discord_ico.png" alt="">
-          </a>
-
-          <a class="li-ic ico">
-            <img src="images/insta_ico.png" alt="">
-          </a>
-
-          <a class="ins-ic ico">
-            <img src="images/snapchat_ico.png" alt="">
-          </a>
-
-          <a class="pin-ic ico">
-            <img src="images/youtube_ico.png" alt="">
-          </a>
         </div>
-      </div>
-      <!-- Grid column -->
 
+        
     </div>
-    <!-- Grid row-->
+    
+  
 
-  </div>
-  <!-- Footer Elements -->
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright:
-    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-  </div>
-  <!-- Copyright -->
+ 
 
 </footer>
 <!-- Footer -->
